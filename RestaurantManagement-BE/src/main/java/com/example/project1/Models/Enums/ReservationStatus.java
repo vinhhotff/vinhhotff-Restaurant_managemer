e0811e0e8bdada8f89d45f7d2109e978ec3e158f
Phonghihi1010 <phonghvqe180015@fpt.edu.vn>
2026-02-02 23:23:50 +0700

    edit enum for field of reservation to lowercase

--- RestaurantManagement-BE/src/main/java/com/example/project1/Models/Enums/ReservationStatus.java
+++ RestaurantManagement-BE/src/main/java/com/example/project1/Models/Enums/ReservationStatus.java
@@ -1,10 +1,28 @@
 package com.example.project1.Models.Enums;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonValue;
+
 public enum ReservationStatus {
-    PENDING,
-    CONFIRMED,
-    CHECKED_IN,
-    CANCELLED,
-    NO_SHOW,
-    COMPLETED
+    pending,
+    confirmed,
+    checked_in,
+    cancelled,
+    no_show,
+    completed;
+
+    @JsonValue
+    public String toValue() {
+        return this.name();
+    }
+
+    @JsonCreator
+    public static ReservationStatus fromValue(String value) {
+        if (value == null) return null;
+        try {
+            return ReservationStatus.valueOf(value.toLowerCase());
+        } catch (IllegalArgumentException e) {
+            return null;
+        }
+    }
 }
